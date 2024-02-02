c9a7c7d13cc735746ce23ec46908821b56f8725f
dhimannikhil300 <121499053+dhimannikhil300@users.noreply.github.com>
2024-02-02 18:01:41 +0000

    conversion (type cast)

---
+++ 02_conversionOperation.js
@@ -0,0 +1,13 @@
+let number = 33
+console.log(typeof number)
+
+let newNumber = Number(number)
+console.log(typeof newNumber)
+console.log(newNumber)
+
+let boolValue = true
+console.log(typeof boolValue)
+
+let newBoolValue = Boolean(boolValue);
+console.log(typeof newBoolValue)
+console.log(newBoolValue)
