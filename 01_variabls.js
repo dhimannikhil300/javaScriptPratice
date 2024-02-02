503ac8afb585c4edb1a97cea7c2400c8d019c3ab
dhimannikhil300 <121499053+dhimannikhil300@users.noreply.github.com>
2024-02-02 17:31:36 +0000

    Variables ( var not recommanded to use due to scope issue and var also has hosting concept) one more way to print that is console.table([]) for more then one value we can use

---
+++ 01_variabls.js
@@ -0,0 +1,7 @@
+// console.log(userName1)
+let userName1 = "Nikhil"
+var userName = "Nikhil"
+const userName2 = "Shivam";
+userNmae3 = "Vivek";
+console.log(userName1)
+console.table([userName, userName2, userName1, userNmae3])
